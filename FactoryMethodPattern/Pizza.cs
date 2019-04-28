17c0becd8e5fbf5901a4af3561c1f50304d480a2
Pandy92 <ndy192@gmail.com>
2019-04-28 19:29:11 +0900

    Factory Method Pattern Example

---
+++ FactoryMethodPattern/Pizza.cs
@@ -0,0 +1,98 @@
+﻿using System;
+namespace Fatory
+{
+    // 주문 가능한 피자 타입들
+    public enum PizzaType
+    {
+        Cheeze,
+        Bulgogi,
+        Vegetable,
+        Sosauge
+    }
+
+    // 추상 클래스
+    public abstract class Pizza
+    {
+        protected string pizzaName = "";
+        protected int cost = 0;
+
+        public void PrintPizza()
+        {
+            Console.WriteLine("Pizza Name : " + pizzaName + " // Pizza Cost : " + cost);
+        }
+    }
+
+    // 도미노 피자
+    public class DominoCheezePizza : Pizza
+    {
+        public DominoCheezePizza()
+        {
+            pizzaName = "DominoCheezePizza";
+            cost = 20;
+        }
+    }
+
+    public class DominoBulgogiPizza : Pizza
+    {
+        public DominoBulgogiPizza()
+        {
+            pizzaName = "DominoBulgogiPizza";
+            cost = 30;
+        }
+    }
+
+    public class DominoVegetablePizza : Pizza
+    {
+        public DominoVegetablePizza()
+        {
+            pizzaName = "DominoVegetablePizza";
+            cost = 40;
+        }
+    }
+
+    public class DominoSosaugePizza : Pizza
+    {
+        public DominoSosaugePizza()
+        {
+            pizzaName = "DominoSosaugePizza";
+            cost = 15;
+        }
+    }
+
+    // 미스터 피
+    public class MrCheezePizza : Pizza
+    {
+        public MrCheezePizza()
+        {
+            pizzaName = "MrCheezePizza";
+            cost = 15;
+        }
+    }
+
+    public class MrBulgogiPizza : Pizza
+    {
+        public MrBulgogiPizza()
+        {
+            pizzaName = "MrBulgogiPizza";
+            cost = 40;
+        }
+    }
+
+    public class MrVegetablePizza : Pizza
+    {
+        public MrVegetablePizza()
+        {
+            pizzaName = "MrVegetablePizza";
+            cost = 18;
+        }
+    }
+
+    public class MrSosaugePizza : Pizza
+    {
+        public MrSosaugePizza()
+        {
+            pizzaName = "MrSosaugePizza";
+            cost = 36;
+        }
+    }
+}
---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Fatory
+{
+
+    /// <summary>
+    /// Pizza store. 피자가게 피자가게마다 피자 제조법은 다르다.
+    /// Factory Method 패턴을 이용하여 객체 생성에 의존성이 낮아진다.
+    /// </summary>
+    abstract class PizzaStore
+    {
+        public Pizza OrderPizza(PizzaType type)
+        {
+            Pizza pizza = CreatePizza(type);
+
+            pizza.PrintPizza();
+
+            return pizza;
+        }
+
+        protected abstract Pizza CreatePizza(PizzaType type);
+    }
+
+    /// <summary>
+    /// Domino pizza. Domino 피자만 만든다
+    /// </summary>
+    class DominoPizza : PizzaStore
+    {
+        protected override Pizza CreatePizza(PizzaType type)
+        {
+            Pizza pizza = null;
+
+            switch (type)
+            {
+                case PizzaType.Cheeze:
+                    pizza = new DominoCheezePizza();
+                    break;
+                case PizzaType.Bulgogi:
+                    pizza = new DominoBulgogiPizza();
+                    break;
+                case PizzaType.Vegetable:
+                    pizza = new DominoVegetablePizza();
+                    break;
+                case PizzaType.Sosauge:
+                    pizza = new DominoSosaugePizza();
+                    break;
+            }
+            return pizza;
+        }
+    }
+
+    /// <summary>
+    /// Mr pizza. Mr 피자만 만든다
+    /// </summary>
+    class MrPizza : PizzaStore
+    {
+        protected override Pizza CreatePizza(PizzaType type)
+        {
+            Pizza pizza = null;
+
+            switch (type)
+            {
+                case PizzaType.Cheeze:
+                    pizza = new MrCheezePizza();
+                    break;
+                case PizzaType.Bulgogi:
+                    pizza = new MrBulgogiPizza();
+                    break;
+                case PizzaType.Vegetable:
+                    pizza = new MrVegetablePizza();
+                    break;
+                case PizzaType.Sosauge:
+                    pizza = new MrSosaugePizza();
+                    break;
+            }
+            return pizza;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // 치즈 피자 주문
+            PizzaType orderPizzaType = PizzaType.Cheeze;
+
+            // DominoPizza 제작
+            PizzaStore store = new DominoPizza();
+
+            store.OrderPizza(orderPizzaType);
+
+            // Mr 피자로 바꿔보기
+            store = new MrPizza();
+            store.OrderPizza(orderPizzaType);
+
+            // 피자 타입 바꿔보기
+            orderPizzaType = PizzaType.Bulgogi;
+            store.OrderPizza(orderPizzaType);
+        }
+    }
+}
