b4219b4a5ebc174454e8f2064f56c0ab2451f8a0
Pandy92 <ndy192@gmail.com>
2019-05-01 16:31:01 +0900

    Iterator Pattern Example

---
+++ IteratorPattern/Iterator.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorEx
+{
+    // Iterator 패턴 
+    // 컨테이너의 종류 상관없이 모든 컨테이너 요소에 접근하는 공통 인터페이스 정의
+    abstract class Iterator<T>
+    {
+        public abstract T Next();
+
+        public abstract bool HasNext();
+
+        public abstract void Remove();
+    }
+
+    // 리스트 Iterator
+    class ListIterator<T> : Iterator<T>
+    {
+        List<T> container;
+        int position = 0;
+
+        public ListIterator(List<T> other)
+        {
+            container = other;
+            this.position = 0;
+        }
+
+        // 다음 요소가 있는지 체크.
+        public override bool HasNext()
+        {
+            if (position >= container.Count || container == null || container.Count == 0)
+                return false;
+
+            return true;
+        }
+
+        // 현재 요소 반환. Postion 다음 으로 이동.
+        public override T Next()
+        {
+            // 컨테이너 넘치면 더 이상 반환하지 않음.
+            if (position > container.Count - 1)
+            {
+                return default(T);
+            }
+
+            T current = container[position];
+            position++;
+
+            // 현재 요소 반환
+            return current;
+        }
+
+        // 현재 위치 요소 제거. 잘 쓸지는 모르겠당
+        public override void Remove()
+        {
+            if (position >= container.Count || container == null || container.Count == 0 || position < 0)
+                return;
+
+            // 현재 위치 요소 제거 후 컨테이너 재정렬.
+            container.RemoveAt(position);
+            container.TrimExcess();
+
+            // 지운 후 현재 위치가 컨테이너 범위 밖이면 위치 이동.
+            if (position > container.Count - 1)
+            {
+                position--;
+            }
+        }
+    }
+
+    // Dictionary Iterator
+    class DictionaryIterator<K, V> : Iterator<V>
+    {
+        Dictionary<K, V> container;
+
+        // Dictionary는 index 접근이 불가하므로 열거자 인터페이스를 멤버변수에 저장해둔다.
+        Dictionary<K, V>.Enumerator enumerator;
+
+        // 반복 횟수 저장
+        int iterCount = 0;
+
+        public DictionaryIterator(Dictionary<K, V> dictionary)
+        {
+            container = dictionary;
+            enumerator = dictionary.GetEnumerator();
+            iterCount = 0;
+        }
+
+        public override bool HasNext()
+        {
+            if (container.Count == 0 || container == null || iterCount > container.Count - 1)
+                return false;
+
+            return true;
+        }
+
+        public override V Next()
+        {
+            // 다음 요소 접근 가능 한지 확인.
+            enumerator.MoveNext();
+
+            // 현재 값 가져옴
+            V current = enumerator.Current.Value;
+
+            // 반복 횟수 증가.
+            iterCount++;
+
+            return current;
+        }
+
+        public override void Remove()
+        {
+            // 현재 요소 키를 가져옴.
+            K currentKey = enumerator.Current.Key;
+            container.Remove(currentKey);
+        }
+    }
+}
---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorEx
+{
+    class MainClass
+    {
+        // 메뉴 가격과 이름이 적혀 있음.
+        class MenuItem
+        {
+            int cost;
+            string name;
+
+            public MenuItem(int cost, string name)
+            {
+                this.cost = cost;
+                this.name = name;
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+
+            public int GetCost()
+            {
+                return cost;
+            }
+        }
+
+        abstract class Menu
+        {
+            public abstract Iterator<MenuItem> CreateIterator();
+        }
+
+        // 저녁 메뉴는 List로 관리중
+        class DinnerMenu : Menu
+        {
+            List<MenuItem> menuList = new List<MenuItem>();
+
+            public void AddItem(MenuItem item)
+            {
+                menuList.Add(item);
+            }
+
+            // List Iterator를 반환 
+            public override Iterator<MenuItem> CreateIterator()
+            {
+                return new ListIterator<MenuItem>(menuList);
+            }
+        }
+
+        // 런치 메뉴는 Dictionary로 관리중
+        class LunchMenu : Menu
+        {
+            Dictionary<int, MenuItem> menuDictionary = new Dictionary<int, MenuItem>();
+
+            public void AddItem(int key, MenuItem item)
+            {
+                menuDictionary.Add(key,item);
+            }
+
+            // Dictionary Iterator 반환
+            public override Iterator<MenuItem> CreateIterator()
+            {
+                return new DictionaryIterator<int ,MenuItem>(menuDictionary);
+            }
+        }
+
+        // 종업원은 Container Iterator만 받고 전체 순회
+        class PartTimer
+        {
+            // Iterator를 받으면 전체 순회 해서 출력함.
+            public static void AllPrintMenu(Iterator<MenuItem> iterator)
+            {
+                while(iterator.HasNext())
+                {
+                    MenuItem item = iterator.Next();
+                    Console.WriteLine(" 메뉴 : " + item.GetName() + " // 가격 : " + item.GetCost());
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            // 점심 메뉴 생성 
+            LunchMenu lunch = new LunchMenu();
+            lunch.AddItem(0, new MenuItem(6000, "제육볶음"));
+            lunch.AddItem(5, new MenuItem(7000, "불고기"));
+            lunch.AddItem(3, new MenuItem(6500, "순두부 찌개"));
+
+            // 저녁 메뉴 생성 
+            DinnerMenu dinner = new DinnerMenu();
+            dinner.AddItem(new MenuItem(15000, "닭발"));
+            dinner.AddItem(new MenuItem(7000, "황도"));
+            dinner.AddItem(new MenuItem(12000, "오뎅탕"));
+
+            PartTimer.AllPrintMenu(lunch.CreateIterator());
+
+            PartTimer.AllPrintMenu(dinner.CreateIterator());
+
+        }
+    }
+}
